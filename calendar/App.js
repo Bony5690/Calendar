4a45418ba7eb5cb9795248369426ec977245a1a8
jerry <jerrybony@yahoo.com>
2019-04-17 12:59:53 -0500

    style: fixed arrows for ios, add close event, and styling fixes

--- calendar/App.js
+++ calendar/App.js
@@ -1,9 +1,14 @@
 
 import React, { Component } from 'react'
-import {  StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
 import CalendarView from './components/calendarView';
+import Product from './components/ProductBlock/assets/Wine.jpg'
+import ProductBlock from './components/ProductBlock/Product-Block';
+import BirthDatePicker from './components/BirthDatePicker/Birth-DatePicker';
 import moment from 'moment';
 
+
+const { width, height } = Dimensions.get('window');
 let calendarDate = moment();
 
 export default class App extends Component {
@@ -11,12 +16,51 @@ export default class App extends Component {
     super(props)
     this.state = {
       selected: calendarDate.format('YYYY-MM-DD'),
-      isVisible: false,
+      isNotVisible: true,
+      showCalendar: false,
+      products: [
+        {
+          id: 1,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+        {
+          id: 2,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+        {
+          id: 3,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+        {
+          id: 4,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+        {
+          id: 5,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+        {
+          id: 6,
+          title: 'Whisker',
+          subtitle: 'Red Wine',
+          image: Product
+        },
+      ]
     }
   }
 
   onShowCalendar = () => {
-    this.setState({ isVisible: !this.state.isVisible })
+    this.setState({ isNotVisible: !this.state.isNotVisible, showCalendar: !this.state.showCalendar })
   }
 
   onDayPress = (day) => {
@@ -31,25 +75,55 @@ export default class App extends Component {
     });
   }
   render() {
-    const { selected, } = this.state;
+    const { selected, products, isNotVisible } = this.state;
     return (
 
       <View style={styles.container}>
+        {/* <BirthDatePicker/> */}
+        {/* <View style={{ flexDirection: 'row', flexWrap: 'wrap', }}>
+          {
+            products.map((product) => (
 
-        {
+              <ProductBlock
+                key={product.id}
+                onPress={() => alert('test')}
+                title={product.title}
+                subtitle={product.subtitle}
+                source={product.image}
+              />
+
+            ))
+
+          }
+        </View> */}
+
+
+
+
+        {/* <Cell
+       title='Whiskers'
+       subtitle='Red Wine'
+       source={require('./components/cell/assets/Wine.jpg')}
+       /> */}
+        {/* {
           this.state.isVisible &&
           <Text style={{ fontSize: 40, color: 'white' }}>{selected}</Text>
-        }
-        <TouchableOpacity
+        } */}
+        {
+          isNotVisible &&
+          <TouchableOpacity
           onPress={this.onShowCalendar}>
-          <Text style={{margin: 10, color: '#ffffff', borderColor: '#ffffff', borderWidth: 0.5, padding: 5}}>Show Calendar</Text>
+          <Text style={{ margin: 10, color: '#ffffff', borderColor: '#ffffff', borderWidth: 0.5, padding: 5 }}>Show Calendar</Text>
         </TouchableOpacity>
 
+        }
+        
         {
-          this.state.isVisible ?
+          this.state.showCalendar ?
 
             <CalendarView
-              markedDates={{ [this.state.selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' } }}
+            onPress={() => this.setState({showCalendar: false, isNotVisible: true})}
+              selected={selected}
               current={selected}
               minDate={calendarDate.format('YYYY-MM-DD')}
               onDayPress={this.onDayPress}
@@ -70,15 +144,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#7a4796',
+
   },
-  calendar: {
-    borderTopWidth: 1,
-    paddingTop: 5,
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-    height: 355,
-    width: 355
-  },
+  // calendar: {
+  //   borderTopWidth: 1,
+  //   paddingTop: 5,
+  //   borderBottomWidth: 1,
+  //   borderColor: '#eee',
+  //   height: 355,
+  //   width: 355
+  // },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
--- calendar/components/calendarView.js
+++ calendar/components/calendarView.js
@@ -1,77 +1,119 @@
 import React from 'react';
 import { Calendar } from 'react-native-calendars'
-import {LocaleConfig} from 'react-native-calendars';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { LocaleConfig } from 'react-native-calendars';
+import Arrow from './Arrow';
+import { View, StyleSheet, Dimensions, Image, TouchableOpacity, TouchableWithoutFeedback, Text } from 'react-native';
 
+const { width } = Dimensions.get('window');
 
 const CalendarView = (props) => {
   LocaleConfig.locales['fr'] = {
-    monthNames: ['January','February','March','April','May','June','August','September','October','November','December'],
-    dayNames: ['Sunday','Monday','Thursday','Wednesday','Thursday','Friday','Saturday'],
-    dayNamesShort: ['SU','MO','TU','WE','TH','FR','SA']
+    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'August', 'September', 'October', 'November', 'December'],
+    dayNames: ['Sunday', 'Monday', 'Thursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    dayNamesShort: ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
   };
   LocaleConfig.defaultLocale = 'fr';
-    return (
-        <View>
-             <Calendar
-           hideExtraDays
-         current={props.current}
-          onDayPress={props.onDayPress}
-          minDate={props.minDate}
-          maxDate={props.maxDate}
-          theme={{
-            textSectionTitleColor:  '#646973',
-            textDayHeaderFontSize: 18,
-            textMonthFontWeight: '700',
-            dayTextColor: '#7a4796',
-            todayTextColor: '#7a4796',
-            selectedDayTextColor: 'white',
-            textDayFontSize: 20,
-            monthTextColor: '#000000',
-            textDisabledColor: 'gray',
-            'stylesheet.calendar.header': {
-              week: {
-                marginTop: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              },
-              arrowImage: {
-                width: 50,
-                height: 50,
-                tintColor: '#7a4796'
-              },
-              arrow:{
-                padding: 0
-              },
-              dayHeader:{
-                fontWeight: '800',
-                fontSize: 18,
-              }
-            },
-            'stylesheet.day.basic': {
-              selected: {
-                borderRadius: 0,
-                backgroundColor: '#878c96'
-              }
-            }
-        
-          }}
-          
-          style={styles.calendar}
-          markedDates={props.markedDates}
 
-        /> 
-        </View>
-    )
-  
+
+
+
+  renderArrow = (direction) => {
+    if (direction === 'left') {
+      return <Image
+        style={{ tintColor: '#7a4796' }}
+        source={require('./arrow.png')} />
+    } else {
+      return <Image
+        style={{ tintColor: '#7a4796' }}
+        source={require('./rightArrow2.png')} />
+    }
+  }
+  return (
+    <View>
+      <TouchableWithoutFeedback
+        style={{ alignContent: 'flex-end' }}
+        onPress={props.onPress}>
+        <Text style={{ color: 'white', fontSize: 25, left: 315, top: -170, position: 'absolute', }}>X</Text>
+      </TouchableWithoutFeedback>
+
+      <Calendar
+        hideExtraDays
+        current={props.current}
+        onDayPress={props.onDayPress}
+        renderArrow={this.renderArrow}
+        minDate={props.minDate}
+        maxDate={props.maxDate}
+        theme={{
+          textSectionTitleColor: '#646973',
+          textDayHeaderFontSize: 18,
+          textMonthFontWeight: '700',
+          dayTextColor: '#7a4796',
+          todayTextColor: '#7a4796',
+          selectedDayTextColor: 'white',
+          textDayFontSize: 20,
+          monthTextColor: '#000000',
+          textDisabledColor: 'gray',
+          'stylesheet.calendar.header': {
+            week: {
+              marginTop: 0,
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              backgroundColor: 'white',
+            },
+            arrowImage: {
+              width: 50,
+              height: 50,
+              tintColor: '#7a4796'
+            },
+            arrow: {
+              padding: 0
+            },
+            dayHeader: {
+              fontWeight: '800',
+              fontSize: 18,
+            },
+            header: {
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingLeft: 10,
+              paddingRight: 10,
+              alignItems: 'center',
+              backgroundColor: 'white'
+            }
+          },
+          'stylesheet.day.basic': {
+            selected: {
+              borderRadius: 0,
+              backgroundColor: '#B0B0B0',
+            },
+            selectedText: {
+              fontWeight: '700',
+              color: '#ffffff'
+            }
+          },
+          'stylesheet.calendar.main': {
+            container: {
+              padding: 12,
+
+            }
+          }
+        }}
+
+        style={styles.calendar}
+        markedDates={{ [props.selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' } }}
+
+      />
+    </View>
+  )
+
 }
 
 
 const styles = StyleSheet.create({
-  
+
   calendar: {
-    height: 375,
-    width: 355
+    width: width,
+    paddingHorizontal: 12,
   }
 });
 
